5ed8bccc1a06fe7207523c4ac5fb6c000c070ea7
Berkovitz <bitay@campus.technion.ac.il>
2023-04-27 16:24:53 +0300

    starting the Node.c

--- Node.c
+++ Node.c
@@ -1,5 +1,34 @@
-//
-// Created by user on 27/04/2023.
-//
+#include <stdlib.h>
 
 #include "Node.h"
+#include "Person.h"
+
+/** Struct declaration */
+
+struct Node_t{
+    Person* person;
+    Node* next;
+    Node* previous;
+};
+
+/**Function Implementations */
+
+Node nodeCreate()
+{
+    Node newNode = (Node)malloc(sizeof(newNode));
+    if(!newNode)
+    {
+        return NULL;
+    }
+    return newNode;
+}
+
+void nodeDestroy(Node* toDestroy, FreePerson free_person)
+{
+    if(toDestroy == NULL)
+    {
+        return;
+    }
+    free_person(toDestroy->person);
+    free(node);
+}

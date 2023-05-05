96e0b0970f9baa9ef9986f98c0ba8906e1db62f8
ayals99 <ayal.stein@campus.technion.ac.il>
2023-05-05 14:54:00 +0300

    implementing Hacker.c

--- Hacker.c
+++ Hacker.c
@@ -1,9 +1,118 @@
 #include "Hacker.h"
 
+#define END_OF_LINE '\n'
+#define END_OF_STRING '0'
+#define ID_LENGTH 10
+#define INT_LENGTH 10
+#define SPACE ' '
+
 struct hacker_t {
+    char* m_hackerId;
     int m_desiredCoursesCount;
     int* m_desiredCoursesArray;
     Friends* m_friends;
     Foes* m_rivals;
 };
 
+int countDesiredCourses(char* secondLine){
+    if(*secondLine == END_OF_LINE){
+        return 0;
+    }
+    int spaceCounter = 0;
+    while(*secondLine != END_OF_LINE){
+        if(*secondLine == SPACE){
+            spaceCounter++;
+        }
+        secondLine++;
+    }
+    return spaceCounter + 1;
+}
+
+Hacker hackerCreate(char* hackerId, int numberOfCourses, int* desiredCourses, Friends* friendsArray, Foes* foesArray){
+    Hacker newHacker = malloc(sizeof(*newHacker));
+    if(newHacker == NULL){
+        return NULL;
+    }
+    newHacker->m_hackerId = hackerId;
+    newHacker->m_desiredCoursesCount = numberOfCourses;
+    newHacker->m_desiredCoursesArray = desiredCourses;
+    newHacker->m_friends = friendsArray;
+    newHacker->m_rivals = foesArray;
+    return newHacker;
+}
+
+void hackerDestroy(Hacker toDestroy){
+
+}
+
+
+/** Getter Functions */
+
+char* getHackerId(Hacker currHacker){
+    if(currHacker == NULL){
+        return NULL;
+    }
+    return currHacker->m_hackerId;
+}
+
+int getCoursesCount(Hacker currHacker){
+    if(currHacker == NULL){
+        return NULL_HACKER;
+    }
+    return currHacker->m_desiredCoursesCount;
+}
+
+int* getCourseArray(Hacker currHacker){
+    if(currHacker == NULL){
+        return NULL;
+    }
+    return currHacker->m_desiredCoursesArray;
+}
+
+Friends* getFriendsArray(Hacker currHacker){
+    if(currHacker == NULL){
+        return NULL;
+    }
+    return currHacker->m_friends;
+}
+
+Foes* getFoesArray(Hacker currHacker){
+    if(currHacker == NULL){
+        return NULL;
+    }
+    return currHacker->m_rivals;
+}
+
+/** Setter Functions */
+
+HackerError setCoursesCount(Hacker currHacker, int courseCount){
+    if(currHacker == NULL){
+        return HACKER_ERROR_BAD_PARA;
+    }
+    currHacker->m_desiredCoursesCount = courseCount;
+    return HACKER_ERROR_SUCCESS;
+}
+
+HackerError setCourseArray(Hacker currHacker, int* courseArray){
+    if(currHacker == NULL){
+        return HACKER_ERROR_BAD_PARA;
+    }
+    currHacker->m_desiredCoursesArray = courseArray;
+    return HACKER_ERROR_SUCCESS;
+}
+
+HackerError setFriendsArray(Hacker currHacker, Friends* friendsArray){
+    if(currHacker == NULL){
+        return HACKER_ERROR_BAD_PARA;
+    }
+    currHacker->m_friends = friendsArray;
+    return HACKER_ERROR_SUCCESS;
+}
+
+HackerError setFoesArray(Hacker currHacker, Foes* foesArray){
+    if(currHacker == NULL){
+        return HACKER_ERROR_BAD_PARA;
+    }
+    currHacker->m_rivals = foesArray;
+    return HACKER_ERROR_SUCCESS;
+}
--- Hacker.h
+++ Hacker.h
@@ -9,6 +9,8 @@
 
 /** Typedef */
 
+#define NULL_HACKER 0
+
 typedef struct hacker_t* Hacker;
 typedef char* Friends;
 typedef char* Foes;
@@ -16,7 +18,9 @@ typedef enum {HACKER_ERROR_ALLOC_FAILED, HACKER_ERROR_BAD_PARA, HACKER_ERROR_SUC
 
 /** Functions */
 
-Hacker hackerCreate(FILE* hackers);
+char* getHackerId(Hacker currHacker);
+
+Hacker hackerCreate(char* hackerId, int numberOfCourses, int* desiredCourses, Friends* friendsArray, Foes* foesArray);
 
 void hackerDestroy(Hacker toDestroy);
 
